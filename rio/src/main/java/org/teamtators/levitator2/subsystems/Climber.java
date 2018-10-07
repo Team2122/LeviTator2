c233318fd4ad436393366bb9025f224e1d8a0930
Sentient Spud <sentientspud@hotmail.com>
2018-10-07 11:36:44 -0600

    Fix climber

--- rio/src/main/java/org/teamtators/levitator2/subsystems/Climber.java
+++ rio/src/main/java/org/teamtators/levitator2/subsystems/Climber.java
@@ -9,7 +9,7 @@ import org.teamtators.common.tester.ManualTestGroup;
 import org.teamtators.common.tester.components.SolenoidTest;
 
 
-public class Climber extends Subsystem implements Configurable<Climber.Config>{
+public class Climber extends Subsystem implements Configurable<Climber.Config> {
     private Solenoid forkSolenoid;
     private Solenoid hookSolenoid;
 
@@ -17,11 +17,11 @@ public class Climber extends Subsystem implements Configurable<Climber.Config>{
         super("Climber");
     }
 
-    public void setForkSolenoidEngage(){
+    public void extendFork() {
         forkSolenoid.set(true);
     }
 
-    public void setHookSolenoidEngage(){
+    public void extendHook() {
         hookSolenoid.set(true);
     }
 
@@ -33,12 +33,12 @@ public class Climber extends Subsystem implements Configurable<Climber.Config>{
         return tests;
     }
 
-    public void configure (Config config){
+    public void configure(Config config) {
         this.forkSolenoid = config.forkSolenoid.create();
         this.hookSolenoid = config.hookSolenoid.create();
     }
 
-    public static class Config{
+    public static class Config {
         public SolenoidConfig forkSolenoid;
         public SolenoidConfig hookSolenoid;
     }
